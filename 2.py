e09012706192db5bef7a72fc725a7b31a5489ae4
student13302 <134705224+student13302@users.noreply.github.com>
2023-05-30 10:46:40 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,61 +1,28 @@
-from tkinter import HIDDEN, NORMAL,Tk, Canvas
-def toggle_eyes():
-    current_color = c.itemcget(eye_left, 'fill')
-    new_color = c.body_color if current_color == 'white' else 'white'
-    current_state = c.itemcget(pupil_left, 'state')
-    new_state = NORMAL if current_state == HIDDEN else HIDDEN
-    c.itemconfigure(pupil_left, state=new_state)
-    c.itemconfigure(pupil_right, state=new_state)
-    c.itemconfigure(eye_left, fill=new_color)
-    c.itemconfigure(eye_right, fill=new_color)
+import turtle
+from itertools import cycle 
 
-def blink():
-    toggle_eyes()
-    root.after(250, toggle_eyes)
-    root.after(3000, blink)
-root=Tk()
+colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 
-c=Canvas(root, width=400, height=400)
-c.configure(bg='hot pink',highlightthickness=0)
 
-c.body_color = 'seashell'
-body = c.create_oval(35, 20, 365, 350, outline=c.body_color, fill=c.body_color)
-ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, outline=c.body_color, fill=c.body_color)
-ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, \
-                             fill=c.body_color)
+def draw_circle(size,angle,shift):
+    turtle.pencolor(next(colors))
+    next_shape =''
+    if shape =='circle':
+        turtle.circle(size)
+        next_shape = 'square'
+    elif shape == 'square':
+        for i in range(4):
+            turtle.forward(size + 2)
+            turtle.left(90)
+        next_shape = 'circle'
+    turtle.right(angle)
+    turtle.forward(shift)
+    draw_circle(size+5, angle+1,shift+1, 'circle')
+    
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(4)
+draw_circle(30, 0, 1, 'circle')
 
-foot_left = c.create_oval(65, 320, 145, 360, outline=c.body_color, fill= c.body_color)
-foot_right = c.create_oval(250, 320, 330, 360, outline=c.body_color, fill= c.body_color)
-pupil_left = c.create_oval(140, 145, 150, 155, outline='black', fill='black')
-eye_right = c.create_oval(230, 110, 260, 170, outline='black', fill='white')
-pupil_right = c.create_oval(240, 145, 250, 155, outline='black', fill='black')
-mouth_normal = c.create_line(170, 250, 200, 272, 230, 250, smooth=1, width=2, state=NORMAL)
-
-mouth_happy = c.create_line(170, 250, 200, 282, 230, 250, smooth=1, width=2, state=HIDDEN)
-mouth_sad = c.create_line(170, 250, 200, 232, 230, 250, smooth=1, width=2, state=HIDDEN)
-cheek_left = c.create_oval(70, 180, 120, 230, outline='red', fill='red', state=HIDDEN)
-cheek_right = c.create_oval(280, 180, 330, 230, outline='red', fill='red', state=HIDDEN)
-
-c.pack()
-
-root.after(1000, blink)
-def show_happy(event):
-    if (20 <= event.x <= 350) and (20 <= event.y <= 350):
-        c.itemconfigure(cheek_left, state=NORMAL)
-        c.itemconfigure(cheek_right, state=NORMAL)
-        c.itemconfigure(mouth_happy, state=NORMAL)
-        c.itemconfigure(mouth_normal, state=HIDDEN)
-        c.itemconfigure(mouth_sad, state=HIDDEN)
-    return
-def hide_happy(event):
-    c.itemconfigure(cheek_left, state=HIDDEN)
-    c.itemconfigure(cheek_right, state=HIDDEN)
-    c.itemconfigure(mouth_happy, state=HIDDEN)
-    c.itemconfigure(mouth_normal, state=NORMAL)
-    c.itemconfigure(mouth_sad, state=HIDDEN)
-    return
-c.bind('<Motion>', show_happy)
-c.bind('<Leave>', hide_happy)
-root.mainloop()
 
 
