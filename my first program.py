d86179faf69fbeee01ac8bbea1a873cec2bed3f7
student13302 <134705224+student13302@users.noreply.github.com>
2023-05-30 10:38:24 +0530

    Add files via upload

---
+++ my first program.py
@@ -0,0 +1,7 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:/Users/tharu/Desktop/puthiya payanam 3/2.py ============
+Hello, World! 
+What is your name?harish
+Hello harish
