b17b7e49024c6fe54d5fc29b71bfc06be516f7d5
student13302 <134705224+student13302@users.noreply.github.com>
2023-06-01 17:56:57 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,119 +1,68 @@
-import random 
-import turtle as t
-t.bgcolor('yellow')
+import turtle as t 
 
-caterpillar = t.Turtle()
-caterpillar.shape('square')
-caterpillar.color('red')
-caterpillar.speed(0)
-caterpillar.penup()
-caterpillar.hideturtle()
-
-leaf = t.Turtle()
-leaf_shape = ((0,0),(14,2),(18,6),(20,20),(6,18),(2,14))
-t.register_shape('leaf',leaf_shape)
-leaf.shape('leaf')
-leaf.color('green')
-leaf.penup()
-leaf.hideturtle()
-leaf.speed(0)
-
-def outside_window():
-    pass
-def game_over():
-    pass
-def display_score(current_score):
-    pass
-def place_left():
-    pass
-
-def start_game():
-    global game_started
-    if game_started:
-        return
-    game_started = true
-
-    score = 0
-    text.turtle.clear()
-
-    caterpillar_speed = 2
-    caterpillar_length = 3
-    caterpillar.shapesize(1,caterpillar_length,1)
-    caterpillar.showturtle()
-    display_score(score)
-place_leaf()
-
-while True:
-    caterpillar.forward(caterpillar_speed)
-    if caterpillar.distance(leaf)<20:
-        place_leaf()
-        caterpillar_length = caterpillar_length + 1
-        caterpillar.shapesize(1,caterpillar_length,1)
-        caterpillar_speed = caterpillar_speed + 1
-        score = score + 10
-        display_score(score)
-    if outside_window():
-        game_over()
-        break
-
-def move_up():
-    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
-        caterpillar.setheading(90)
-
-def move_down():
-    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
-        caterpillar.setheading(270)
-
-def move_left():
-    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
-        caterpillar.setheading(180)
-
-def move_right():
-    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
-        caterpillar.setheading(0)
-
-t.onkey(start_game,'space')
-t.listen()
-t.mainloop()
-
-def outside_window():
-    left_wall = -t.window_width()/2
-    right_wall = t.window_width()/2
-    top_wall = t.window_height()/2
-    bottom_wall = -t.window_height()/2
-    (x,y) = caterpillar.pos()
-    outside = x < left_wall or  x > right_wall or  y < bottom_wall or y > top_wall
-    return outside
-
-def game_over():
-    caterpillar.color('yellow')
-    leaf.color('yellow')
+def rectangle(horizontal,vertical,color):
+    t.pendown()
+    t.pensize(1)
+    t.color(color)
+    t.begin_fill()
+    for counter in range(1,3):
+        t.forward(horizontal)
+        t.right(90)
+        t.forward(vertical)
+        t.right(90)
+    t.end_fill()
     t.penup()
-    t.hideturtle()
-    t.write('GAME OVER!',align='center' , font=('Aerial',30,'normal'))
 
-def display_score(current_score):
-    score_turtle.clear()
-    score_turtle.penup()
-    x = (t.window_width() / 2)-50
-    y = (t.window_height() / 2)-50
-    score_turtle.setpos(x,y)
-    score_turtle.write(str(current_score) , align = 'right',font=('Arial',40,'bold'))
+t.penup()
+t.speed('slow')
+t.bgcolor('Hot Pink')
 
-t.onkey(start_game,'space')
-t.onkey(move_up,'Up')
-t.onkey(move_right,'Right')
-t.onkey(move_down,'Down')
-t.onkey(move_left,'Left')
-t.listen()
-t.mainloop()
+#feet
+t.goto(-100, -150)
+rectangle(50,20,'yellow')
+t.goto(-30,-150)
+rectangle(50,20,'yellow')
 
-def place_leaf():
-    leaf.hideturtle()
-    leaf.setx(rd.randint(-200,200))
-    leaf.sety(rd.randint(-200,200))
-    leaf.showturtle()
+#legs
+t.goto(-25, -50)
+rectangle(15,100,'purple')
+t.goto(-55,-50)
+rectangle(-15,100,'puple')
 
+#body
+t.goto(-90,100)
+rectangle(100,150,'goldenrod')
 
+#arms
+t.goto(-150, 70)
+rectangle(60,15,'light blue')
+t.goto(-150,110)
+rectangle(15,40,'light blue')
 
-    
+t.goto(10, 70)
+rectangle(60,15,'light blue')
+t.goto(55,110)
+rectangle(15,40,'light blue')
+
+#neck
+t.goto(-50,120)
+rectangle(15,20,'thistle')
+
+#head
+t.goto(-85,170)
+rectangle(80,50,'bue')
+
+#eyes
+t.goto(-60, 160)
+rectangle(30,10,'white')
+t.goto(-60,160)
+rectangle(5,5,'black')
+t.goto(-45,155)
+rectangle(5,5,'black')
+
+#mouth
+t.goto(-65,135)
+t.right(5)
+rectangle(40,5,'black')
+
+t.hideturtle()
